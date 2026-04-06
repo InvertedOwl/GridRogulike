2f09a4a083b91b1ca341eed8c5ac57dc2e16803c
InvertedOwl <commandking123@gmail.com>
2026-04-05 20:36:53 -0600

    minor changes

--- Assets/Scripts/RunInfo.cs
+++ Assets/Scripts/RunInfo.cs
@@ -18,7 +18,7 @@ public class RunInfo : MonoBehaviour
     public Button redrawButton;
     public List<TextMeshProUGUI> difficultyText;
     public List<TextMeshProUGUI> stepsText;
-    public static string seed = new Random().Next().ToString();
+    public static string seed = "test";
     public readonly int combineCost = 2;
 
 
